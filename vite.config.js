ba959a6a1b47f84f832ee2d7b38cede4b77a2b1d
chinotrb <chinotrb@gmail.com>
2025-11-22 13:02:37 -0600

    5

--- vite.config.js
+++ vite.config.js
@@ -19,17 +19,18 @@ export default defineConfig({
         background_color: '#ffffff',
         icons: [
           {
-            src: 'icon-192.png',
+            src: '/En-Viaje/icon-192.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: 'icon-512.png',
+            src: '/En-Viaje/icon-512.png',
             sizes: '512x512',
             type: 'image/png'
           }
         ]
       }
+
     })
   ]
 })
